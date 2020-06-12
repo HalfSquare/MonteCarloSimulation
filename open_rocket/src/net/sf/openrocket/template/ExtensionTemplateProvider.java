315f3fd550756c53de229336d36ad58300bc4596
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-06-12 17:29:39 +1200

    Commented out template, began hooking into OpenRocket. Now creating a abstract listener and importing a rocket - the code is there but is currently breaking on the import statement.

--- open_rocket/src/net/sf/openrocket/template/ExtensionTemplateProvider.java
+++ open_rocket/src/net/sf/openrocket/template/ExtensionTemplateProvider.java
@@ -1,15 +1,15 @@
-package net.sf.openrocket.template;
-
-import net.sf.openrocket.plugin.Plugin;
-import net.sf.openrocket.simulation.extension.AbstractSimulationExtensionProvider;
-import net.sf.openrocket.simulation.extension.SimulationExtension;
-
-/**
- *	Defines where the extension is displayed in the menu and what class.
- */
-@Plugin
-public class ExtensionTemplateProvider extends AbstractSimulationExtensionProvider {
-
-	public ExtensionTemplateProvider() { super(ExtensionTemplate.class, "Extension templates", "Template"); }
-
-}
+//package net.sf.openrocket.template;
+//
+//import net.sf.openrocket.plugin.Plugin;
+//import net.sf.openrocket.simulation.extension.AbstractSimulationExtensionProvider;
+//import net.sf.openrocket.simulation.extension.SimulationExtension;
+//
+///**
+// *	Defines where the extension is displayed in the menu and what class.
+// */
+//@Plugin
+//public class ExtensionTemplateProvider extends AbstractSimulationExtensionProvider {
+//
+//	public ExtensionTemplateProvider() { super(ExtensionTemplate.class, "Extension templates", "Template"); }
+//
+//}
