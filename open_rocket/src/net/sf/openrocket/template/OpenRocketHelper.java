2cd00144cb5a929ac5b6babc3e0cf39bdb0b1511
Michael Behan <behanmich@ecs.vuw.ac.nz>
2020-06-12 16:10:26 +1200

    Startup OpenRocket programically
    - Add OpenRocketHelper.java, main method calls Startup method from OpenRocket

---
+++ open_rocket/src/net/sf/openrocket/template/OpenRocketHelper.java
@@ -0,0 +1,13 @@
+package net.sf.openrocket.template;
+
+import net.sf.openrocket.startup.Startup;
+
+public class OpenRocketHelper {
+
+    public static void main(String[] args) throws Exception {
+        Startup.main(args);
+//        Simulation sim = new Simulation(new Rocket());
+//        sim.getSimulationListeners().add("net.sf.openrocket.simulation.listeners.example.AirStart");
+    }
+
+}
