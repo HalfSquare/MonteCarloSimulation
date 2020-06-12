46d4d9230d6c0e13b02d92c5f2e60781f48a6a79
Alexander Pace <pacealex@ecs.vuw.ac.nz>
2020-06-12 14:11:06 +1200

    Add re-run button
    Returns to simulation settings window.
    Co-authored-by: Max <mcmurrmax@ecs.vuw.ac.nz>

--- gui/src/GraphWindow.java
+++ gui/src/GraphWindow.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class GraphWindow {
   private JComboBox comboBox1;
   private JButton saveToFileButton;
   private JPanel rootPanel;
   private JButton exitButton;
+  private JButton reRunButton;
+
+  /**
+   * Constructor.
+   */
+  public GraphWindow(){
+    exitButton.addActionListener(new ExitAction());
+  }
 
   public JPanel getRootPanel() {
     return rootPanel;
+  }
+
+  public void setReRunButtonListener(ActionListener listener) {
+    this.reRunButton.addActionListener(listener);
   }
 
   public void setVisible(boolean flag) {
