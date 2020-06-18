ff083a8a7f5fc58c49185374e081bbc1bbc52baa
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-06-18 17:57:30 +1200

    Refactor test class Renamed test class With @mcmurrmax

--- src/test/java/TestOpenRocket.java
+++ src/test/java/TestOpenRocket.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  *
  */
 
-public class OpenRocketTest1 {
+public class TestOpenRocket {
 	// Initial test to set up pipeline - should ALWAYS pass
 	@Test
 	public void Test1(){
