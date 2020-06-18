4b5d2dd47fd54763bdf7ffc474e3f127b90cfafe
GStrongman <georgia.strongman@gmail.com>
2020-06-18 16:07:13 +1200

    Tweaked tests Added printlns to tests to help debug pipeline.

--- ORtests/OpenRocketTest1.java
+++ ORtests/OpenRocketTest1.java
@@ -19,11 +19,13 @@ public class OpenRocketTest1 {
 		int x = 1;
 		int y = 1;
 		assertEquals(x, y);
+		System.out.println("Happy noises 1");
 	}
 
 	// Initial test to set up pipeline - should ALWAYS pass
 	@Test
 	public void Test2(){
 		assertTrue(true);
+		System.out.println("Happy noises 2");
 	}
 }
