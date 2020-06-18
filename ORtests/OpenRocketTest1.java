089b8f9cf79fb1c483d7c8e3c8530a26a1f6dec5
GStrongman <georgia.strongman@gmail.com>
2020-06-18 15:41:47 +1200

    Adds inital tests Set up JUNIT and created some tests that should ALWAYS pass, to help set up pipeline.

---
+++ ORtests/OpenRocketTest1.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * These tests are designed to test OpenRocket to make sure everything is working as expected,
+ * and that no bugs have been introduced to the code.
+ *
+ * @author Georgia
+ *
+ */
+
+public class OpenRocketTest1 {
+	// Initial test to set up pipeline - should ALWAYS pass
+	@Test
+	public void Test1(){
+		int x = 1;
+		int y = 1;
+		assertEquals(x, y);
+	}
+
+	// Initial test to set up pipeline - should ALWAYS pass
+	@Test
+	public void Test2(){
+		assertTrue(true);
+	}
+}
