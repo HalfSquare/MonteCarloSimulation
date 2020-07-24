1de7d0e10ebbce1f09bee1ecb91a6e74625c8c3a
pacealex <pacealex@ecs.vuw.ac.nz>
2020-07-24 15:20:36 +1200

    Add "Save graph to image" button skeleton
    Will save the current graph as a PNG.
    Action not yet implemented.

---
+++ target/classes/Template/ExtensionTemplateSimulationListener.java
@@ -0,0 +1,10 @@
+//package net.sf.openrocket.template;
+//
+//import net.sf.openrocket.simulation.listeners.AbstractSimulationListener;
+//
+///**
+// * Simulation listener, can override listeners and make them call methods.
+// */
+//public class ExtensionTemplateSimulationListener extends AbstractSimulationListener {
+//
+//}
