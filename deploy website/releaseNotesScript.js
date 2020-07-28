d3430eb8554841dc53dd7f7f657a3ca421b109ed
Michael Behan <behanmich@ecs.vuw.ac.nz>
2020-07-28 18:36:18 +1200

    Add release script to get milestones

--- deploy website/releaseNotesScript.js
+++ deploy website/releaseNotesScript.js
@@ -1,11 +1,29 @@
-const url = "https://gitlab.ecs.vuw.ac.nz/api/v4/projects/6908/milestones?state=closed";
+let https = require('https');
 
 const options = {
+    hostname: "gitlab.ecs.vuw.ac.nz",
+    path: "/api/v4/projects/6908/milestones?state=closed",
+    method: 'GET',
     headers: {
-        Authorization: "Bearer $CI_JOB_TOKEN"
+        Authorization: "Bearer $CI_JOB_TOKEN",
+        contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
     }
 };
 
-fetch(url, options)
-    .then( res => res.json() )
-    .then( data => console.log(data) );
+const req = https.request(options, (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    resp.on('end', () => {
+        let json = JSON.parse(data)
+        console.log(json[0]);
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
+
+req.end();
