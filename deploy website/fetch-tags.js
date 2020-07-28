79fb1ce2e2304e2a66f874fe0cb210dfca918bb2
Max McMurray <mcmurrmax@ecs.vuw.ac.nz>
2020-07-28 23:12:23 +1200

    Add file that updates the release json file

---
+++ deploy website/fetch-tags.js
@@ -0,0 +1,30 @@
+console.log("run");
+let https = require('https');
+
+const options = {
+    hostname: "gitlab.ecs.vuw.ac.nz",
+    path: "/api/v4/projects/6908/tags",
+    method: 'GET',
+    headers: {
+        Authorization: "Bearer $CI_JOB_TOKEN",
+        contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+};
+
+const req = https.request(options, (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    resp.on('end', () => {
+        let json = JSON.parse(data)
+        console.log(json);
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
+
+req.end();
