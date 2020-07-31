77a3a4bd0da425f7a1073bbd09af25ce9bf3cede
Michael Behan <behanmich@ecs.vuw.ac.nz>
2020-07-31 15:49:14 +1200

    Add getToken function to pass token and make api calls

--- deploy website/parseMilestoneJson.js
+++ deploy website/parseMilestoneJson.js
@@ -1,7 +1,49 @@
-var fs = require('fs'), filename = process.argv[2];
+let https = require('https');
 
-fs.readFile(filename, 'utf8', function(err, data) {
-    if (err) throw err;
-    const id = JSON.parse(data)[0]["id"]
-    fs.writeFileSync(filename, String(id));
+function getToken () {
+    let args = null;
+    process.argv
+        .slice(2, process.argv.length)
+        .forEach( arg => {
+            // long arg
+            if (arg.slice(0,2) === '--') {
+                const longArg = arg.split('=');
+                const longArgValue = longArg.length > 1 ? longArg[1] : true;
+                args = longArgValue;
+            }
+        });
+    return args;
+}
+
+const token = getToken();
+
+const options = {
+    hostname: "gitlab.ecs.vuw.ac.nz",
+    path: "/api/v4/projects/6908/milestones?state=closed",
+    method: 'GET',
+    headers: {
+        Authorization: "Bearer $CI_JOB_TOKEN",
+        Authorization: 'Bearer '+token,
+        contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+};
+
+console.log(options)
+
+const req = https.request(options, (resp) => {
+    let data = ''
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    resp.on('end', () => {
+        let json = JSON.parse(data)
+        console.log(json[0]);
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
 });
+
+req.end();
+
