ac0b50d1884f61fbabc7f253f8ae00360ac2b072
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-08-06 17:55:47 +1200

    Add documentation to Mission Control Settings
    In digging through OpenRocket's code and forums I've found information about the settings and what measurement formats they should be in. Some of the information is assumed, so caution is advised.

--- src/main/java/nz/ac/vuw/engr301/group15/gui/MissionControlSettings.java
+++ src/main/java/nz/ac/vuw/engr301/group15/gui/MissionControlSettings.java
@@ -1,5 +1,41 @@
 package nz.ac.vuw.engr301.group15.gui;
 
+/**
+ Mission Control Settings contains the values that can be inputted into the simulation to
+ change the simulation results to be more accurate to flight conditions.
+
+
+ Max Angle: maximum step angle of the rocket. Should be converted to radians before handing to OpenRocket - should be
+ between (0.017453292519943295, 0.3490658503988659), or between (1, 20) degrees otherwise OpenRocket will clamp to
+ these values.
+
+ Wind Direction:
+
+ Wind Turbulence:
+
+ Wind Speed:
+
+ Launch Temperature: temperature of the launch site. Read in as a celcius value, should be converted to kelvin
+ before handing to OpenRocket.
+
+ Launch Air Pressure:
+
+ Launch Rod Length: this is probably measured in metres, must be between (0.0, 1.0471975511965976)
+ otherwise OpenRocket will clamp to these values.
+
+ Launch Rod Direction:
+
+ Launch Rod Angle:
+
+ Launch Altitude:
+
+ Launch Latitude:
+
+ Launch Longitude:
+
+ Number of Simulations:
+ */
+
 public class MissionControlSettings {
   private String maxAngle;
   private String windDir;
