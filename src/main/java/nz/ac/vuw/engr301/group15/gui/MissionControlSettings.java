2e9f5874bee2459c797711c8d7274b6282c3e009
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-08-06 23:04:15 +1200

    Add documentation to Mission Control Settings
    Final addition to documentation for now, measurement types and how they are handled by OpenRocket have been covered to the best of my ability.

--- src/main/java/nz/ac/vuw/engr301/group15/gui/MissionControlSettings.java
+++ src/main/java/nz/ac/vuw/engr301/group15/gui/MissionControlSettings.java
@@ -4,22 +4,6 @@ package nz.ac.vuw.engr301.group15.gui;
  Mission Control Settings contains the values that can be inputted into the simulation to
  change the simulation results to be more accurate to flight conditions.
 
- Excerpt from OpenRocket Developer's Wiki:
- 	Units used in OpenRocket
- 	OpenRocket always uses internally pure SI units. For example all rocket dimensions and flight distances are in
- 	meters, all masses are in kilograms, density is in kg/m³, temperature is in Kelvin etc. This convention is also
- 	used when storing the design in the OpenRocket format.
-
- 	The only exception to this rule is angles:
-
- 	Angles are represented as radians internally, but in the file format they are converted to degrees. This is to make
- 	the file format more human-readable and to avoid rounding errors.
- 	Latitude and longitude of the launch site are represented in degrees both internally and externally.
- 	When displaying measures to the user, the values are converted into the preferred units of the user. This is
- 	performed using classes in the package net.sf.openrocket.unit. The Unit class represents a single unit and it
- 	includes methods for converting between that unit and SI units in addition to creating a string representation with
- 	a suitable amount of decimals. A UnitGroup describes a measurable quantity such as temperature and contains the units
- 	available for that quantity, such as Celcius, Fahrenheit and Kelvin.
 
  Max Angle: maximum step angle of the rocket. Should be converted to radians before handing to OpenRocket - should be
   between (0.017453292519943295, 0.3490658503988659), or between (1, 20) degrees otherwise OpenRocket will clamp to
@@ -29,30 +13,33 @@ package nz.ac.vuw.engr301.group15.gui;
   does not - this is odd, and may need to be added in. Currently not sure which direction 0 degrees is - assumption is
   North-facing.
 
- Wind Turbulence: wind turbulence intensity of launch site, used to calculate wind speed deviation (average * turbulence). Percentage
-  value, 100% = 2.0 m/s standard deviation, 10% = 0.2 m/s standard deviation.
+ Wind Turbulence: wind turbulence intensity of launch site, used to calculate wind speed deviation (average * turbulence).
+ 	Percentage value, 100% = 2.0 m/s standard deviation, 10% = 0.2 m/s standard deviation - however should be handed to
+ 	OpenRocket as a decimal.
 
  Wind Speed: average wind speed of launch site, measured in m/s.
 
- Launch Temperature: temperature of the launch site. Read in as a celcius value, should be converted to kelvin
+ Launch Temperature: temperature of the launch site. Read in as a Celcius value, should be converted to kelvin
   before handing to OpenRocket.
 
- Launch Air Pressure:
+ Launch Air Pressure: air pressure of the launch site, used in conjunction with launch temperature to calculate atmosphere,
+ 	measured in mbar (Millibar Pressure Unit).
 
  Launch Rod Length: this is probably measured in metres, must be between (0.0, 1.0471975511965976)
   otherwise OpenRocket will clamp to these values.
 
- Launch Rod Direction:
+ Launch Rod Direction: direction of the rocket, measured in radians, assumption is that 0 is North-facing.
 
- Launch Rod Angle: max 1.0471975511965976
+ Launch Rod Angle: tilt of the rocket downwards, measured in radians, assumption is that 0 is straight sky-facing.
+ 	Max value: 1.0471975511965976
 
- Launch Altitude:
+ Launch Altitude: altitude of the launch site, measured in metres.
 
  Launch Latitude: standard coordinate measurement, South values should be negative and North values positive.
 
  Launch Longitude: standard coordinate measurement, West values should be negative and East values positive.
 
- Number of Simulations: number of Monte Carlo simulations to be run
+ Number of Simulations: number of Monte Carlo simulations to be run.
  */
 
 public class MissionControlSettings {
