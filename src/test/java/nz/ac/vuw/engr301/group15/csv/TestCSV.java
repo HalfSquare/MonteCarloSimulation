4fec15c6d4057bdec91702cfc797cc7e72497533
Michael Behan <behanmich@ecs.vuw.ac.nz>
2020-09-01 14:47:38 +1200

    CSV console import Test
    co-author: @Dongjacq

--- src/test/java/nz/ac/vuw/engr301/group15/csv/TestCSV.java
+++ src/test/java/nz/ac/vuw/engr301/group15/csv/TestCSV.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.io.InputStream;
+import nz.ac.vuw.engr301.group15.gui.Gui;
 import org.junit.jupiter.api.Test;
 
 
@@ -28,6 +29,52 @@ public class TestCSV {
 		File file = new File("points.csv");
 		assertTrue(file.exists());
 	}
+
+	/**
+	 * Test 1: CSV imported correctly
+	 */
+	@Test
+	public void TestCSVConsoleImport()  {
+		Gui gui = new Gui(false, new File("src/main/resources/testMCData.csv"));
+		String numSims = gui.settingsMissionControl.getNumSimulations();
+		String rodAngle = gui.settingsMissionControl.getLaunchRodAngle();
+		String rodLength = gui.settingsMissionControl.getLaunchRodLength();
+		String rodDir = gui.settingsMissionControl.getLaunchRodDir();
+		String launchAlt = gui.settingsMissionControl.getLaunchAlt();
+		String launchLat = gui.settingsMissionControl.getLaunchLat();
+		String launchLong = gui.settingsMissionControl.getLaunchLong();
+		String maxAngle = gui.settingsMissionControl.getMaxAngle();
+		String windSpeed = gui.settingsMissionControl.getWindSpeed();
+		String windDir = gui.settingsMissionControl.getWindDir();
+		String windTurbulence = gui.settingsMissionControl.getWindTurbulence();
+		String launchTemp = gui.settingsMissionControl.getLaunchTemp();
+		String airPressure = gui.settingsMissionControl.getLaunchAirPressure();
+		assertEquals(numSims, "10");
+		assertEquals(rodAngle, "0");
+		assertEquals(rodLength, "0.2");
+		assertEquals(rodDir, "0");
+		assertEquals(launchAlt, "0");
+		assertEquals(launchLat, "30");
+		assertEquals(launchLong, "-50");
+		assertEquals(maxAngle, "0.018");
+		assertEquals(windSpeed, "3");
+		assertEquals(windDir, "0");
+		assertEquals(windTurbulence, "0.1");
+		assertEquals(launchTemp, "285");
+		assertEquals(airPressure, "1010");
+	}
+
+	/**
+	 * Test 2: CSV exported correctly
+	 */
+
+	/**
+	 * Test 3: Application runs with GUI
+	 */
+
+	/**
+	 * Test 4: Application runs without GUI
+	 */
 
 
 }
