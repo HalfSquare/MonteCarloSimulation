bc3732c78066c123d6031d52be25887e65851e91
Jacqui Dong <dongjacq@ecs.vuw.ac.nz>
2020-09-01 14:18:36 +1200

    Check file exists test
    co-authored @behanmich

--- src/test/java/nz/ac/vuw/engr301/group15/csv/TestCSV.java
+++ src/test/java/nz/ac/vuw/engr301/group15/csv/TestCSV.java
@@ -1,80 +1,33 @@
-package nz.ac.vuw.engr301.group15.montecarlo;
+package nz.ac.vuw.engr301.group15.csv;
 
-import java.io.InputStream;
-import nz.ac.vuw.engr301.group15.gui.Gui;
-import nz.ac.vuw.engr301.group15.gui.MissionControlSettings;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
 
 
 /**
- * These tests are designed to test OpenRocket to make sure everything is working as expected,
+ * These tests are designed to test the CSV output of the API to make sure everything is working as expected,
  * and that no bugs have been introduced to the code.
  *
- * @author Georgia
+ * @author Michael & Jacqui
  *
  */
 
-public class TestOpenRocket {
-	MissionControlSettings settings = new MissionControlSettings();
-	// Initial test to set up pipeline - should ALWAYS pass
+
+public class TestCSV {
+
+	/**
+	 * This tests the CSV Output file has been created
+	 */
 	@Test
-	public void Test1(){
-		int x = 1;
-		int y = 1;
-		assertEquals(x, y);
+	public void TestFileCreated(){
+		File file = new File("points.csv");
+		assertTrue(file.exists());
 	}
 
-	// Initial test to set up pipeline - should ALWAYS pass
-	@Test
-	public void Test2(){
-		assertTrue(true);
-	}
-
-	// Initial test to check that simulation can run 1 instance
-	@Test
-	public void Test3() {
-		MonteCarloSimulation sim = new MonteCarloSimulation();
-		try{
-			ClassLoader classLoader = this.getClass().getClassLoader();
-			InputStream rocketFile = classLoader.getResourceAsStream("rocket-1-1-9.ork");
-			sim.runSimulations(rocketFile, settings);
-		}
-		catch (Exception ex){
-			fail();
-		}
-	}
-
-	// Initial test to check that simulation will not crash on negative value
-	@Test
-	public void Test4(){
-		MonteCarloSimulation sim = new MonteCarloSimulation();
-		try {
-			ClassLoader classLoader = this.getClass().getClassLoader();
-			InputStream rocketFile = classLoader.getResourceAsStream("rocket-1-1-9.ork");
-			sim.runSimulations(rocketFile, settings);
-		}
-		catch (Exception ex){
-			fail();
-		}
-	}
-
-	// Initial test to check that simulation can run 10 instances
-	@Test
-	public void Test5() {
-		MonteCarloSimulation sim = new MonteCarloSimulation();
-		try{
-			ClassLoader classLoader = this.getClass().getClassLoader();
-			InputStream rocketFile = classLoader.getResourceAsStream("rocket-1-1-9.ork");
-			sim.runSimulations(rocketFile, settings);
-		}
-		catch (Exception ex){
-			ex.printStackTrace();
-			fail();
-		}
-	}
 
 }
