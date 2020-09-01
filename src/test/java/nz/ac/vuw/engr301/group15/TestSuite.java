53e37613a2b98bf7f9889f0f0ef7ef4940074f95
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-09-01 15:51:33 +1200

    Change junit version to 4.12 with @mcmurrmax
    This will change the version of junit to 4.12 from 5.3.1 to accommodate OpenRocket's test suite.

--- src/test/java/nz/ac/vuw/engr301/group15/TestSuite.java
+++ src/test/java/nz/ac/vuw/engr301/group15/TestSuite.java
@@ -3,28 +3,34 @@ package nz.ac.vuw.engr301.group15;
 import nz.ac.vuw.engr301.group15.gui.TestMissionControlData;
 import nz.ac.vuw.engr301.group15.montecarlo.TestMonteCarlo;
 import nz.ac.vuw.engr301.group15.openrocket.TestOpenRocket;
+import nz.ac.vuw.engr301.group15.openrocket.arch.TestSystemInfo;
+import nz.ac.vuw.engr301.group15.openrocket.unit.TestValue;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
+/**
+ *  Ignore this
+ */
 
-@Suite.SuiteClasses({
-		// GUI tests
-		TestMissionControlData.class,
-
-		// Monte Carlo tests
-		TestMonteCarlo.class,
-
-		//OpenRocket tests
-		TestOpenRocket.class,
-
-		//Arch tests
-		//nz.ac.vuw.engr301.group15.montecarlo.openrocket.arch.TestSystemInfo.class
-
-		//Unit tests
-		//nz.ac.vuw.engr301.group15.openrocket.unit.TestValue.class
-
-})
+//@RunWith(Suite.class)
+//
+//@Suite.SuiteClasses({
+//		// GUI tests
+//		TestMissionControlData.class,
+//
+//		// Monte Carlo tests
+//		TestMonteCarlo.class,
+//
+//		//OpenRocket tests
+//		TestOpenRocket.class,
+//
+//		//Arch tests
+//		TestSystemInfo.class,
+//
+//		//Unit tests
+//		TestValue.class
+//
+//})
 
 
 
