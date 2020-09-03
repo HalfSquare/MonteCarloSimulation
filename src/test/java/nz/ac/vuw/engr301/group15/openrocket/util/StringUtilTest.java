225623db613c80d8898dfa392e8a7ed3e3100cd6
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-09-04 11:47:29 +1200

    Add OpenRocket unit tests
    Adds junit tests to check OpenRocket is functioning correctly.

---
+++ src/test/java/nz/ac/vuw/engr301/group15/openrocket/util/StringUtilTest.java
@@ -0,0 +1,25 @@
+package nz.ac.vuw.engr301.group15.openrocket.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * A class that tests
+ * {@link net.sf.openrocket.util.StringUtil}.
+ */
+public class StringUtilTest {
+    @Test
+    public void testStrings() {
+        assertTrue(StringUtil.isEmpty(""));
+        assertTrue(StringUtil.isEmpty(new StringBuilder().toString())); // ""
+        assertTrue(StringUtil.isEmpty(" "));
+        assertTrue(StringUtil.isEmpty("  "));
+        assertTrue(StringUtil.isEmpty("       "));
+        assertTrue(StringUtil.isEmpty(null));
+
+        assertFalse(StringUtil.isEmpty("A"));
+        assertFalse(StringUtil.isEmpty("         .        "));
+    }
+}
