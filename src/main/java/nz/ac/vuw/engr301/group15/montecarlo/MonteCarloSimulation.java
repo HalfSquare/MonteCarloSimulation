0ef5f17784b3a8a6f7e5f02080403342e782c04d
Michael Behan <behanmich@ecs.vuw.ac.nz>
2020-09-11 14:42:16 +1200

    Update to Open Rocket 15

--- src/main/java/nz/ac/vuw/engr301/group15/montecarlo/MonteCarloSimulation.java
+++ src/main/java/nz/ac/vuw/engr301/group15/montecarlo/MonteCarloSimulation.java
@@ -1,14 +1,20 @@
 package nz.ac.vuw.engr301.group15.montecarlo;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 import java.io.InputStream;
 import net.sf.openrocket.document.OpenRocketDocument;
 import net.sf.openrocket.document.Simulation;
 import net.sf.openrocket.file.RocketLoadException;
+import net.sf.openrocket.gui.main.Splash;
+import net.sf.openrocket.gui.main.SwingExceptionHandler;
+import net.sf.openrocket.plugin.PluginModule;
 import net.sf.openrocket.rocketcomponent.Rocket;
 import net.sf.openrocket.simulation.SimulationOptions;
 import net.sf.openrocket.simulation.SimulationStatus;
-import net.sf.openrocket.startup.Startup;
-import net.sf.openrocket.startup.Startup2;
+import net.sf.openrocket.startup.Application;
+import net.sf.openrocket.startup.GuiModule;
 import nz.ac.vuw.engr301.group15.gui.MissionControlSettings;
 
 import java.io.File;
@@ -98,9 +104,8 @@ public class MonteCarloSimulation {
 
       simulationListener.reset();
       helper.runSimulation(simulation, simulationListener);
-
       while (simulationListener.getSimulation() == null) {
-        System.out.println("waiting");
+//        System.out.println("waiting");
       }
       String progress = String.format("%.2f", (simNum/numOfSimulations)*100.0);
       System.out.print("Simulating: " + progress + "% " + animationChars[loadingSpinIndex] + "\r");
@@ -138,16 +143,30 @@ public class MonteCarloSimulation {
 
   public MonteCarloSimulation() {
     this(null);
-    Startup.initializeLogging();
-    Startup.initializeL10n();
-    Startup2.loadMotor();
+    Splash.init();
+    SwingExceptionHandler exceptionHandler = new SwingExceptionHandler();
+    Application.setExceptionHandler(exceptionHandler);
+    exceptionHandler.registerExceptionHandler();
+    // Load motors etc.log.info("Loading databases");
+    GuiModule guiModule = new GuiModule();
+    Module pluginModule = new PluginModule();
+    Injector injector = Guice.createInjector(guiModule, pluginModule);
+    Application.setInjector(injector);
+    guiModule.startLoader();
   }
 
   public MonteCarloSimulation(Runnable runnable) {
     this.listener = runnable;
-    Startup.initializeLogging();
-    Startup.initializeL10n();
-    Startup2.loadMotor();
+    Splash.init();
+    SwingExceptionHandler exceptionHandler = new SwingExceptionHandler();
+    Application.setExceptionHandler(exceptionHandler);
+    exceptionHandler.registerExceptionHandler();
+    // Load motors etc.log.info("Loading databases");
+    GuiModule guiModule = new GuiModule();
+    Module pluginModule = new PluginModule();
+    Injector injector = Guice.createInjector(guiModule, pluginModule);
+    Application.setInjector(injector);
+    guiModule.startLoader();
   }
 
   public static void main(String[] args) {
