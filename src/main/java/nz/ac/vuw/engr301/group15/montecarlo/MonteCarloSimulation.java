b2840a73fc06dd9b285602d9b695c37ebc85beca
Georgia Strongman <stronggeor@ecs.vuw.ac.nz>
2020-09-11 14:24:16 +1200

    Update PID rocket file
    The fin set must be called 'CONTROL' (case sensitive) to work (no, I'm not kidding, OpenRocket 'just works')

--- src/main/java/nz/ac/vuw/engr301/group15/montecarlo/MonteCarloSimulation.java
+++ src/main/java/nz/ac/vuw/engr301/group15/montecarlo/MonteCarloSimulation.java
@@ -205,7 +205,7 @@ public class MonteCarloSimulation {
        */
       MonteCarloSimulation mcs = new MonteCarloSimulation();
       ClassLoader classLoader = mcs.getClass().getClassLoader();
-      InputStream rocketFile = classLoader.getResourceAsStream("rocket-1-1-9.ork");
+      InputStream rocketFile = classLoader.getResourceAsStream("pid_rocket.ork");
       mcs.runSimulationsPID(rocketFile, 1);
     } catch (RocketLoadException e) {
       e.printStackTrace();
