0036295ebe9e21070854ad95319e3ab9c3784970
justinakoh <justina.y.koh@gmail.com>
2020-09-18 02:54:38 +1200

    Setup the API for the tomtommap api

--- src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
+++ src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
@@ -1,52 +1,20 @@
 package nz.ac.vuw.engr301.group15.montecarlo;
 
-import eu.jacquet80.minigeo.MapWindow;
-import eu.jacquet80.minigeo.POI;
-import eu.jacquet80.minigeo.Point;
-import eu.jacquet80.minigeo.Segment;
+public class Map{
+  String API_Key = "KrGZcmPxmu4tNuWChwMteQNlNADrcByh";
 
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+  //URL example
+//  http://api.tomtom.com/map/1/staticimage?key=Your_API_Key&zoom=9&center=13.567893,46.112341&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=en-GB
 
-public class Map {
-  private static Pattern POINT = Pattern.compile("^.*?(-?\\d+\\.\\d+)\\s+(-?\\d+\\.\\d+)$");
 
-  public static void main(String[] args) throws IOException {
-    MapWindow window = new MapWindow();
+  /**
+   * Method which formulates the URL to be sent to the REST API 
+   * @return
+   */
+  public String createURL(){
+    String URL;
 
-    BufferedReader r = new BufferedReader(new FileReader("C:\\Users\\Justina\\Desktop\\2020\\Semester 1\\ENGR301\\group-15\\src\\main\\java\\nz\\ac\\vuw\\engr301\\group15\\montecarlo\\france.txt"));
-    String line;
-    Point cur, prec = null;
-    int readCount = 0;
-    int errCount = 0;
-    double latitide = 67.52388;
-    double longitude = 21.87598;
-
-//    window.addSegment();
-//    while((line = r.readLine()) != null) {
-//      readCount++;
-//      Matcher m = POINT.matcher(line);
-//      if(m.matches()) {
-//        double lon = Double.parseDouble(m.group(1));
-//        double lat = Double.parseDouble(m.group(2));
-//        cur = new Point(lat, lon);
-//        if(prec != null) window.addSegment(new Segment(prec, cur, lon<0 ? Color.BLUE : Color.RED));
-//        prec = cur;
-//      } else errCount++;
-//    }
-
-    window.addPOI(new POI(new Point(latitide, longitude), "Yo?"));
-
-    System.out.println("Read " + readCount + " lines; ignored " + errCount);
-
-    window.setVisible(true);
+    return URL;
   }
-
-
-
 }
+
