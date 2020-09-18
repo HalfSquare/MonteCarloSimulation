fcb715b63573911472e770a713904b99ab515a49
justinakoh <justina.y.koh@gmail.com>
2020-09-18 12:17:43 +1200

    Test URL is correct and working

--- src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
+++ src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
@@ -1,26 +1,28 @@
 package nz.ac.vuw.engr301.group15.montecarlo;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Map{
-  String API_Key = "KrGZcmPxmu4tNuWChwMteQNlNADrcByh";
-  int zoom = 9;
-  double cenLeft = 13.567893;
-  double cenRight = 46.112341;
-  String format = "jpg";
-  String layer = "basic";
-  String style = "main";
-  long width = 1305;
-  long height = 748;
-  String view = "Unified";
-  String language = "en-GB";
+  static String API_Key = "KrGZcmPxmu4tNuWChwMteQNlNADrcByh";
+  static int zoom = 9;
+  static double cenLeft = 13.567893;
+  static double cenRight = 46.112341;
+  static String format = "jpg";
+  static String layer = "basic";
+  static String style = "main";
+  static long width = 500;
+  static long height = 250;
+  static String view = "Unified";
+  static String language = "en-GB";
 
   /**
    * Method which formulates the URL to be sent to the REST API
    * http://api.tomtom.com/map/1/staticimage?
    * key=Your_API_Key&
-   * zoom=17&
+   * zoom=9&
    * center=13.567893,46.112341&
    * format=jpg&
    * layer=basic&
@@ -31,11 +33,11 @@ public class Map{
    * language=en-GB
    * @return
    */
-  public URL createURL() throws MalformedURLException {
+  public static URL createURL() throws MalformedURLException {
     String urlString = "http://api.tomtom.com/map/1/staticimage?" +
             "key=" + API_Key + "&" +
             "zoom=" + zoom + "&" +
-            "center=" + cenLeft + "," + cenRight +
+            "center=" + cenLeft + "," + cenRight + "&" +
             "format=" + format + "&" +
             "layer=" + layer + "&" +
             "style=" + style + "&" +
