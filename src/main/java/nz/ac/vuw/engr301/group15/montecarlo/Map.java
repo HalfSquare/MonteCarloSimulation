6c6a5f91c198096aae8b7128181a3b306ef2109f
justinakoh <justina.y.koh@gmail.com>
2020-09-18 16:28:31 +1200

    Create method which clacultes the angle from the coordinate

--- src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
+++ src/main/java/nz/ac/vuw/engr301/group15/montecarlo/Map.java
@@ -54,6 +54,23 @@ public class Map extends JPanel {
     System.out.println("URL IS: " + urlString);
     return new URL(urlString);
   }
+  
+  private double angleFromCoordinate(double lat1, double long1, double lat2, double long2) {
+
+    double dLon = (long2 - long1);
+
+    double y = Math.sin(dLon) * Math.cos(lat2);
+    double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
+            * Math.cos(lat2) * Math.cos(dLon);
+
+    double brng = Math.atan2(y, x);
+
+    brng = Math.toDegrees(brng);
+    brng = (brng + 360) % 360;
+    brng = 360 - brng; // count degrees counter-clockwise - remove to make clockwise
+
+    return brng;
+  }
 
   public static BufferedImage createMap() throws IOException {
     URL url = createURL();
